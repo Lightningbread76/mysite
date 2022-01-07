1b9bd4d776f541f652ac8beb8eff33242d6b31e0
Kevin Guo <67837759+Lightningbread76@users.noreply.github.com>
2022-01-06 19:42:29 -0500

    created build

---
+++ react-website-yt-1/build/precache-manifest.4b4ac309b2c44513a287edcc5e8ea6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42ee231d556430a71b285d7d1210f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c335015187ec6ac78ca2",
+    "url": "/static/css/main.d3549954.chunk.css"
+  },
+  {
+    "revision": "449d1057b3b1e6709580",
+    "url": "/static/js/2.8091b14b.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.8091b14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c335015187ec6ac78ca2",
+    "url": "/static/js/main.7fab983c.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
